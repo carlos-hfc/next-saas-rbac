98b5fca489371d9be1dc21e21433cab17d9aa0e3
Carlos Faustino <chfcchfc96@gmail.com>
2024-04-24 15:49:22 -0300

    feat: add create account

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,66 @@
+import { hash } from "bcryptjs"
+import type { FastifyInstance } from "fastify"
+import type { ZodTypeProvider } from "fastify-type-provider-zod"
+import { z } from "zod"
+
+import { prisma } from "@/lib/prisma"
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/users",
+    {
+      schema: {
+        tags: ["Auth"],
+        summary: "Create a new account",
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { email, name, password: passwordHash } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          message: "User with same e-mail already exists",
+        })
+      }
+
+      const [, domain] = email.split("@")
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
+      const password = await hash(passwordHash, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          password,
+          memberOn: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
+        },
+      })
+
+      return reply.status(201).send()
+    }
+  )
+}

fba3390f9d6dad6fd20cb20bea7ff109c789226d
Carlos Faustino <chfcchfc96@gmail.com>
2024-04-25 09:42:04 -0300

    feat: add bearer auth on swagger

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -17,6 +17,7 @@ export async function getProfile(app: FastifyInstance) {
         schema: {
           tags: ["Auth"],
           summary: "Get authenticated user profile",
+          security: [{ bearerAuth: [] }],
           response: {
             200: z.object({
               user: z.object({

19d2b0feba16151935d0507dc0911d5fded23ab8
Carlos Faustino <chfcchfc96@gmail.com>
2024-06-17 17:15:18 -0300

    feat: add theme switcher

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 
 import type { Metadata } from "next"
+import { ThemeProvider } from "next-themes"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,8 +13,19 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className="dark">{children}</body>
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }

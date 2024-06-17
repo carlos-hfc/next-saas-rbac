fc30bb4684f3fdf92d6bb1a6f274408d4984c594
Carlos Faustino <chfcchfc96@gmail.com>
2024-06-17 16:29:50 -0300

    feat: get organizations

---
+++ apps/web/src/http/get-organizations.ts
@@ -0,0 +1,18 @@
+import { api } from "./api-client"
+
+interface GetOrganizationsResponse {
+  organizations: {
+    id: string
+    name: string
+    slug: string
+    avatarUrl: string | null
+  }[]
+}
+
+export async function getOrganizations() {
+  const response = await api
+    .get("organizations")
+    .json<GetOrganizationsResponse>()
+
+  return response
+}

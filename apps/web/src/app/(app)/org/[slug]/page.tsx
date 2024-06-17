3ab6f28d6a5a21d0d8903263bcb59b4009bea936
Carlos Faustino <chfcchfc96@gmail.com>
2024-06-17 16:45:31 -0300

    feat: show organization selected

---
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/header"
+
+export default async function Projects() {
+  return (
+    <div className="py-4">
+      <Header />
+
+      <main></main>
+    </div>
+  )
+}
